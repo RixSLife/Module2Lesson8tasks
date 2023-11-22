23011cb2d1db0ef9eed1de6f8e51253555a3144c
RixSLife <150831287+RixSLife@users.noreply.github.com>
2023-11-22 17:06:50 +0000

    Task 3 done

--- Module2Lesson8tasks/Program.cs
+++ Module2Lesson8tasks/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Module2Lesson8tasks
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Module2Lesson8tasks
 {
     internal class Program
     {
@@ -14,59 +17,73 @@
             //Napisz program, który zaimplementuje ciąg Fibonacciego i wyświetli go na ekranie.
             Console.WriteLine("\n");
             Console.WriteLine("Zadanie nr 3\n");
-            Console.WriteLine("Zaimplementuj ciag Fibonacciego.");
+            Console.WriteLine("Podaj do liczbe do ktorej chcesz wyswietlic ciag Fibonacciego:");
+            string userInput3 = Console.ReadLine();
+            int x3;
+            int.TryParse(userInput3, out x3);
             int a3 = 1;
             int b3 = 1;
-            while (a3 < 500)
+
+            while (a3 < x3)
             {
-                int c3 = a3 + b3;
-                Console.WriteLine($"{a3} + {b3} = {c3}");
-                b3 = a3 + b3;
-                a3++;
+                int c3 = b3 + a3;
+                int d3 = c3 + b3;
+                int e3 = d3 + c3;
+                int f3 = e3 + d3;
+                int g3 = f3 + e3;
+                Console.WriteLine($"{a3}");
+                Console.WriteLine($"{b3}");
+                Console.WriteLine($"{c3}");
+                Console.WriteLine($"{d3}");
+                Console.WriteLine($"{e3}");
+                Console.WriteLine($"{f3}");
+                Console.WriteLine($"{g3}");
+                a3 = g3 + f3;
+                b3 = a3 + g3;
             }
+        
+        //Zadanie nr 4
+        //Napisz program, który po podaniu liczby całkowitej wyświetli piramidę liczb od 1 do podanej liczby w formie jak poniżej:
+        //1
+        //2 3
+        //4 5 6
+        //7 8 9 10
 
+        //Zadanie nr 5
+        //Napisz program, który dla liczb od 1 do 20 wyświetli na ekranie ich 3 potęgę.
 
-            //Zadanie nr 4
-            //Napisz program, który po podaniu liczby całkowitej wyświetli piramidę liczb od 1 do podanej liczby w formie jak poniżej:
-            //1
-            //2 3
-            //4 5 6
-            //7 8 9 10
+        //Zadanie nr 6
+        //Napisz program, który dla liczb od 0 do 20 obliczy sumę wg wzoru:
+        //1 + ½ +1 / 3 + ¼ itd.
 
-            //Zadanie nr 5
-            //Napisz program, który dla liczb od 1 do 20 wyświetli na ekranie ich 3 potęgę.
+        //Zadanie nr 7
+        //Napisz program, który dla liczby zadanej przez użytkownika narysuje diament o krótszej przekątnej o długości wprowadzonej przez użytkownika i wg wzoru:
+        //    *
+        //   ***
+        //  *****
+        // *******
+        //*********
+        // *******
+        //  *****
+        //   ***
+        //    *
 
-            //Zadanie nr 6
-            //Napisz program, który dla liczb od 0 do 20 obliczy sumę wg wzoru:
-            //1 + ½ +1 / 3 + ¼ itd.
+        //Zadanie nr 8
+        //Napisz program, który odwróci wprowadzony przez użytkownika ciąg znaków.
+        //Np.Testowe dane:
+        //Abcdefg
+        //Rezultat
+        //Gfedcba
 
-            //Zadanie nr 7
-            //Napisz program, który dla liczby zadanej przez użytkownika narysuje diament o krótszej przekątnej o długości wprowadzonej przez użytkownika i wg wzoru:
-            //    *
-            //   ***
-            //  *****
-            // *******
-            //*********
-            // *******
-            //  *****
-            //   ***
-            //    *
+        //Zadanie nr 9
+        //Napisz program, który zamieni liczbę dziesiętną na liczbę binarną.
 
-            //Zadanie nr 8
-            //Napisz program, który odwróci wprowadzony przez użytkownika ciąg znaków.
-            //Np.Testowe dane:
-            //Abcdefg
-            //Rezultat
-            //Gfedcba
-
-            //Zadanie nr 9
-            //Napisz program, który zamieni liczbę dziesiętną na liczbę binarną.
-
-            //Zadanie nr 10
-            //Napisz program, który znajdzie najmniejszą wspólną wielokrotność dla zadanych 2 liczb.
-
-            Console.WriteLine();
-            Console.ReadKey();
+        //Zadanie nr 10
+        //Napisz program, który znajdzie najmniejszą wspólną wielokrotność dla zadanych 2 liczb.
         }
+
     }
-}
+
+ }
+
+
