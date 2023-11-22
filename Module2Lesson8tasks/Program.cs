94962b2983a19a82f30d0a42bc7e45d095c74b09
RixSLife <150831287+RixSLife@users.noreply.github.com>
2023-11-22 13:38:39 +0000

    Add project files.

---
+++ Module2Lesson8tasks/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Module2Lesson8tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zadanie nr 1
+            //Napisz program, który sprawdzi ile jest liczb pierwszych w zakresie 0 – 100.
+
+            //Zadanie nr 2
+            //Napisz program, w którym za pomocą pętli do…while znajdziesz wszystkie liczby parzyste z zakresu 0 – 1000.
+
+            //Zadanie nr 3
+            //Napisz program, który zaimplementuje ciąg Fibonacciego i wyświetli go na ekranie.
+
+            //Zadanie nr 4
+            //Napisz program, który po podaniu liczby całkowitej wyświetli piramidę liczb od 1 do podanej liczby w formie jak poniżej:
+            //1
+            //2 3
+            //4 5 6
+            //7 8 9 10
+
+            //Zadanie nr 5
+            //Napisz program, który dla liczb od 1 do 20 wyświetli na ekranie ich 3 potęgę.
+
+            //Zadanie nr 6
+            //Napisz program, który dla liczb od 0 do 20 obliczy sumę wg wzoru:
+            //1 + ½ +1 / 3 + ¼ itd.
+
+            //Zadanie nr 7
+            //Napisz program, który dla liczby zadanej przez użytkownika narysuje diament o krótszej przekątnej o długości wprowadzonej przez użytkownika i wg wzoru:
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //*********
+            // *******
+            //  *****
+            //   ***
+            //    *
+
+            //Zadanie nr 8
+            //Napisz program, który odwróci wprowadzony przez użytkownika ciąg znaków.
+            //Np.Testowe dane:
+            //Abcdefg
+            //Rezultat
+            //Gfedcba
+
+            //Zadanie nr 9
+            //Napisz program, który zamieni liczbę dziesiętną na liczbę binarną.
+
+            //Zadanie nr 10
+            //Napisz program, który znajdzie najmniejszą wspólną wielokrotność dla zadanych 2 liczb.
+
+
+            Console.ReadKey();
+        }
+    }
+}
