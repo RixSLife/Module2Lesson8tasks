2879000d5405747276cdec4a3d5051f043a0c58b
RixSLife <150831287+RixSLife@users.noreply.github.com>
2023-11-22 17:26:39 +0000

    Task 1 started

--- Module2Lesson8tasks/Program.cs
+++ Module2Lesson8tasks/Program.cs
@@ -9,6 +9,15 @@ namespace Module2Lesson8tasks
         {
             //Zadanie nr 1
             //Napisz program, który sprawdzi ile jest liczb pierwszych w zakresie 0 – 100.
+            Console.WriteLine("\n");
+            Console.WriteLine("Zadanie nr 1\n");
+            Console.WriteLine("Ponizej program wyswietli ilosc liczb pierwszych w zakresie 0-100:");
+            int a = 1;
+
+            while (a == 101)
+            {
+
+            }
 
             //Zadanie nr 2
             //Napisz program, w którym za pomocą pętli do…while znajdziesz wszystkie liczby parzyste z zakresu 0 – 1000.
@@ -40,48 +49,52 @@ namespace Module2Lesson8tasks
                 Console.WriteLine($"{g3}");
                 a3 = g3 + f3;
                 b3 = a3 + g3;
+
             }
-        
-        //Zadanie nr 4
-        //Napisz program, który po podaniu liczby całkowitej wyświetli piramidę liczb od 1 do podanej liczby w formie jak poniżej:
-        //1
-        //2 3
-        //4 5 6
-        //7 8 9 10
 
-        //Zadanie nr 5
-        //Napisz program, który dla liczb od 1 do 20 wyświetli na ekranie ich 3 potęgę.
+            //Zadanie nr 4
+            //Napisz program, który po podaniu liczby całkowitej wyświetli piramidę liczb od 1 do podanej liczby w formie jak poniżej:
+            //1
+            //2 3
+            //4 5 6
+            //7 8 9 10
 
-        //Zadanie nr 6
-        //Napisz program, który dla liczb od 0 do 20 obliczy sumę wg wzoru:
-        //1 + ½ +1 / 3 + ¼ itd.
+            //Zadanie nr 5
+            //Napisz program, który dla liczb od 1 do 20 wyświetli na ekranie ich 3 potęgę.
 
-        //Zadanie nr 7
-        //Napisz program, który dla liczby zadanej przez użytkownika narysuje diament o krótszej przekątnej o długości wprowadzonej przez użytkownika i wg wzoru:
-        //    *
-        //   ***
-        //  *****
-        // *******
-        //*********
-        // *******
-        //  *****
-        //   ***
-        //    *
+            //Zadanie nr 6
+            //Napisz program, który dla liczb od 0 do 20 obliczy sumę wg wzoru:
+            //1 + ½ +1 / 3 + ¼ itd.
 
-        //Zadanie nr 8
-        //Napisz program, który odwróci wprowadzony przez użytkownika ciąg znaków.
-        //Np.Testowe dane:
-        //Abcdefg
-        //Rezultat
-        //Gfedcba
+            //Zadanie nr 7
+            //Napisz program, który dla liczby zadanej przez użytkownika narysuje diament o krótszej przekątnej o długości wprowadzonej przez użytkownika i wg wzoru:
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //*********
+            // *******
+            //  *****
+            //   ***
+            //    *
 
-        //Zadanie nr 9
-        //Napisz program, który zamieni liczbę dziesiętną na liczbę binarną.
+            //Zadanie nr 8
+            //Napisz program, który odwróci wprowadzony przez użytkownika ciąg znaków.
+            //Np.Testowe dane:
+            //Abcdefg
+            //Rezultat
+            //Gfedcba
 
-        //Zadanie nr 10
-        //Napisz program, który znajdzie najmniejszą wspólną wielokrotność dla zadanych 2 liczb.
-        }
+            //Zadanie nr 9
+            //Napisz program, który zamieni liczbę dziesiętną na liczbę binarną.
 
+            //Zadanie nr 10
+            //Napisz program, który znajdzie najmniejszą wspólną wielokrotność dla zadanych 2 liczb.
+
+
+
+        } 
+  
     }
 
  }
