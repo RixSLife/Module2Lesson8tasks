3282ffd10eba3dacf9a756b453a4a1cc39a878bd
RixSLife <150831287+RixSLife@users.noreply.github.com>
2023-11-28 01:58:51 +0000

    Task 2 completed

--- Module2Lesson8tasks/Program.cs
+++ Module2Lesson8tasks/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Channels;
 
 namespace Module2Lesson8tasks
 {
@@ -9,26 +10,31 @@ namespace Module2Lesson8tasks
         {
             //Zadanie nr 1
             //Napisz program, który sprawdzi ile jest liczb pierwszych w zakresie 0 – 100.
-           // Console.WriteLine("\n");
-           // Console.WriteLine("Zadanie nr 1\n");
-           // Console.WriteLine("Ponizej program wyswietli ilosc liczb pierwszych w zakresie 0-100:");
-           // int a = 1;
-
-          //  while (a == 101)
-            {
-          //      if (b = a % a)
-          //      Console.WriteLine($"{b}");
-          //      a++;
-            }
+            Console.WriteLine("Zadanie nr 1\n");
+            Console.WriteLine("Ponizej program wyswietli ilosc liczb pierwszych w zakresie 0-100:");
 
             //Zadanie nr 2
             //Napisz program, w którym za pomocą pętli do…while znajdziesz wszystkie liczby parzyste z zakresu 0 – 1000.
+            Console.WriteLine("\nZadanie nr 2\n");
+            Console.WriteLine("Program wyswietli wszystkie liczby parzyste w zakresie 0-1000:");
+            int i = 1;
+            while (i < 1001)
+            {
+                if ((i % 2) == 0)
+                {
+                    Console.Write($" {i}");
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
 
             //Zadanie nr 3
             //Napisz program, który zaimplementuje ciąg Fibonacciego i wyświetli go na ekranie.
-            Console.WriteLine("\n");
-            Console.WriteLine("Zadanie nr 3\n");
-            Console.WriteLine("Podaj do liczbe do ktorej chcesz wyswietlic ciag Fibonacciego:");
+            Console.WriteLine("\nZadanie nr 3\n");
+            Console.WriteLine("Podaj ilosc wartosci ciag Fibonacciego ktore chcesz wyswietlic:");
             string userInput3 = Console.ReadLine();
             int x3;
             int.TryParse(userInput3, out x3);
@@ -51,6 +57,9 @@ namespace Module2Lesson8tasks
             //2 3
             //4 5 6
             //7 8 9 10
+            Console.WriteLine("\nZadanie nr 4\n");
+            Console.WriteLine("Podaj liczbe calkowita do ktorej zbudujemy piramide:");
+            int userInput4 = int.Parse(Console.ReadLine());
 
             //Zadanie nr 5
             //Napisz program, który dla liczb od 1 do 20 wyświetli na ekranie ich 3 potęgę.
